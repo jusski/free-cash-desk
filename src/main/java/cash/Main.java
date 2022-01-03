e24bc4e965f53f197cc30b1adc991ef03052f6ec
jusski <jusski@sdf-eu.org>
2022-01-03 18:34:13 +0200

    added consoleutils to main

--- src/main/java/cash/Main.java
+++ src/main/java/cash/Main.java
@@ -1,13 +1,53 @@
 package cash;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import lombok.val;
 
 public class Main {
-    public static void main (String[] args) {
-        for (int i = 0; i < 5000; ++i) {
+    public static void main (String[] args) throws InterruptedException {
+    	new Thread(new Runnable()
+		{
+			
+			@Override
+			public void run()
+			{
+				 ConsoleUtils.cls();
+			        Collection<Cashier> cashiers = FastFoodRestaurant.getCashiers();
+			        while(true)
+			        {
+			        	int counter = 0;
+			        	for(Cashier cashier : cashiers)
+			        	{
+			        		counter += 1;
+			        		int queueLength = cashier.getQueueLength();
+			        		char[] stars = new char[queueLength];
+			        		Arrays.fill(stars, '*');
+			        		ConsoleUtils.printf(0, counter, new String(stars));
+			        		
+			        		
+			        	}
+			        	try
+						{
+							Thread.sleep(33);
+						}
+						catch (InterruptedException e)
+						{
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+			        }
+				
+			}
+		}).start();
+    	
+        for (int i = 0; i < 500; ++i) {
             val visitor = new Customer(i);
             new Thread(visitor).start();
         }
         ((Cashier)FastFoodRestaurant.getCashiers().toArray()[2]).speed = 100;
+        
+       
     }
 }
