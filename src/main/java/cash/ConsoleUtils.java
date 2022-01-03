9318e850e2d488e44a37a537911f3d444bf3e8aa
jusski <jusski@sdf-eu.org>
2022-01-03 18:39:38 +0200

    returned consoleutils.java

---
+++ src/main/java/cash/ConsoleUtils.java
@@ -0,0 +1,29 @@
+package cash;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class ConsoleUtils
+{
+    static public native void setCursorCoord(int x, int y);
+    static public native void cls();
+    static public native void printf(int x, int y,  String string);
+    
+    static
+    {
+    	System.load("C:/LIB/Console/console.dll");
+    }
+    
+    public static void main(String[] args) throws InterruptedException
+	{
+		cls();
+		//setCursorCoord(1, 20);
+		int counter = 0;
+		while(true) {
+			Thread.sleep(100);
+			ConsoleUtils.printf(0, 20, "liaw liaw liaw%12d " + counter++);
+			ConsoleUtils.printf(20, 24, "liaw " + counter++);
+			cls();
+			System.out.println("Hi");
+		}
+	}
+}
